99878cb1e5b282d604d1a0c424698da6bc754458
nujood <nojoodweb@gmail.com>
2017-12-20 12:50:56 +0300

    delete yaml editor

--- drush/drushrc.php
+++ drush/drushrc.php
@@ -6,3 +6,6 @@
 if (file_exists(__DIR__ . '/contrib/drush-shell-aliases/drushrc.php')) {
     include __DIR__ . '/contrib/drush-shell-aliases/drushrc.php';
   }
+
+  $command_specific['config-export']['skip-modules'] = ['yaml_editor'];
+  $command_specific['config-import']['skip-modules'] = ['yaml_editor'];
